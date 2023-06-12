74d7178cf1f6079932efc2fb91b001524cca8282
Victor Yoalli <victoryoalli@gmail.com>
2023-06-12 15:38:34 -0600

    wip

--- app/views/index.view.php
+++ app/views/index.view.php
@@ -4,7 +4,7 @@
     <h1>Home Page</h1>
     <button @click="open = !open" class="bg-blue-500 px-4 py-1.5">Open Modal</button>
     <div x-show="open" class="fixed inset-0 z-10 flex items-center justify-center bg-black bg-opacity-50">
-        <div class="p-8 bg-white rounded-lg">
+        <div class="p-8 bg-white rounded-lg" @click.outside="open=false">
             <h2>Modal Title</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
             <button class="text-xs bg-gray-300" @click="open = false">Close</button>
